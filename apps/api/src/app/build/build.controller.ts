079bd761bb00ef7b75a098a5fd86d8067214db40
WindSekirun <windsekirun@gmail.com>
2024-05-11 23:03:21 +0900

    빌드 로그 업데이트 구현

--- apps/api/src/app/build/build.controller.ts
+++ apps/api/src/app/build/build.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Logger, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { BuildService } from './build.service';
 import { ApiExtraModels, ApiResponse, getSchemaPath } from '@nestjs/swagger';
-import { PipelineResult } from '@nx-prisma-nestjs-example/model/prisma/pipeline_result'
+import { PipelineResult } from '@nx-prisma-nestjs-example/model/prisma/pipeline_result';
+import { LogContent } from '@nx-prisma-nestjs-example/model/LogContent';
 
 @Controller()
 export class BuildController {
@@ -9,13 +10,15 @@ export class BuildController {
 
   @Post('build/:buildId/analysis/register')
   async registerResult(@Param('buildId') buildId: string) {
-    Logger.debug(buildId);
     return this.buildService.registerBuild(buildId);
   }
 
   @Post('build/:buildId/analysis/log')
-  async updateLog(@Param('buildId') buildId: string) {
-    Logger.debug(buildId);
+  async updateLog(
+    @Param('buildId') buildId: string,
+    @Body() logContent: LogContent
+  ) {
+    return this.buildService.updateBuildLog(buildId, logContent);
   }
 
   @Post('build/:buildId/analysis/unit')
@@ -24,16 +27,15 @@ export class BuildController {
   }
 
   @Get('build')
-  async getBuildList() {
-  }
+  async getBuildList() {}
 
   @ApiExtraModels(PipelineResult)
   @ApiResponse({
-     status: 200,
-     schema: {
-       $ref: getSchemaPath(PipelineResult),
-     },
-   })
+    status: 200,
+    schema: {
+      $ref: getSchemaPath(PipelineResult),
+    },
+  })
   @Get('build/:buildId')
   async getBuild(@Param('buildId') buildId: string) {
     Logger.debug(buildId);
--- apps/api/src/app/build/build.service.ts
+++ apps/api/src/app/build/build.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { LogContent } from '@nx-prisma-nestjs-example/model/LogContent';
 
 @Injectable()
 export class BuildService {
@@ -9,6 +10,32 @@ export class BuildService {
     return await this.prisma.pipelineResult.create({
       data: {
         buildId: buildId,
+      },
+    });
+  }
+
+  async updateBuildLog(buildId: string, logContent: LogContent) {
+    const pipelineResult = await this.prisma.pipelineResult.findUnique({
+      where: { buildId },
+      include: {
+        BuildLog: true,
+      },
+    });
+
+    if (!pipelineResult) {
+      throw new Error('PipelineResult not found');
+    }
+
+    return await this.prisma.buildLog.upsert({
+      where: {
+        pipelineResultId: pipelineResult.id,
+      },
+      update: {
+        log: logContent.log,
+      },
+      create: {
+        log: logContent.log,
+        pipelineResultId: pipelineResult.id,
       },
     });
   }
