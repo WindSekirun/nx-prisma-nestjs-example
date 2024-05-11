409b360661f8faaad1c623a2975930b55fb8e000
WindSekirun <windsekirun@gmail.com>
2024-05-11 15:53:23 +0900

    데이터 저장을 위한 기본 데이터 구조 작성

--- apps/api/src/app/prisma/extended-client.ts
+++ apps/api/src/app/prisma/extended-client.ts
@@ -1,11 +1,22 @@
-
-import { PrismaClient } from "@prisma/client";
-
-import { existsExtension } from "./extensions/exists.extension";
+import { PrismaClient } from '@prisma/client';
+import { existsExtension } from './extensions/exists.extension';
+import cuid2Extension from 'prisma-extension-cuid2';
 
 function extendClient(base: PrismaClient) {
   // Add as many as you'd like - no ugly types required!
-  return base.$extends(existsExtension);
+  return base.$extends(existsExtension).$extends(
+    cuid2Extension({
+      // 정의하지 않은 경우 *:id 패턴을 사용하여 예상치 않은 버그를 일으킬 수 있으므로
+      // 가능하면 cuid2를 사용할 모든 id를 정의하는 것이 좋음
+      fields: [
+        'PipelineResult:id',
+        'UnitTestResult:id',
+        'TestClass:id',
+        'TestFunction:id',
+        'BuildLog:id',
+      ],
+    })
+  );
 }
 
 class UntypedExtendedClient extends PrismaClient {
